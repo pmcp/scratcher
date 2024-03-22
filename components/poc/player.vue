ecf4435a2a4972397e7fffaf5f6e5f2075a8b4dd
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-03-22 18:54:55 +0100

    init PoC setup

---
+++ components/poc/player.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useMediaControls } from '@vueuse/core'
+
+const video = ref()
+const { playing, currentTime, duration, volume } = useMediaControls(video, {
+  src: 'barracuda.m4a',
+})
+
+// Change initial media properties
+onMounted(() => {
+  volume.value = 0.5
+  currentTime.value = 60
+})
+</script>
+
+<template>
+  <video ref="video" />
+  <button @click="playing = !playing">
+    Play / Pause
+  </button>
+  <span>{{ currentTime }} / {{ duration }}</span>
+</template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,22 @@
 <template>
   <main>
-    <ContentDoc />
+<!--    <ContentDoc />-->
+    <PocPlayer />
+    <div
+    >pos: {{ x
+      }}, {{ y
+      }}</div
+    >
   </main>
 </template>
+
+
+<script setup lang="ts">
+
+
+const { x
+  , y
+} = useMouse
+()
+</script>
+ate>
